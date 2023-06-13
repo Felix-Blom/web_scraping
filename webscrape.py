1ff9374a7543d28f29f64f7cb3cc2c16fd36c910
Felix-Blom <felix.blom@live.nl>
2023-06-13 21:48:29 +0200

    creating!

---
+++ webscrape.py
@@ -0,0 +1,67 @@
+import asyncio
+from asyncio import sleep
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+
+service = ChromeService(executable_path="C:\webdrivers\chromedriver.exe")
+d = webdriver.Chrome(service=service)
+
+d.get(
+    "https://www.cbr.nl/nl/rijbewijs-houden/nl/gezondheidsverklaring/zoek-een-specialist.htm"
+)
+
+"""Set Location to Amsterdam"""
+location = d.find_element(
+    By.XPATH, "//input[@id='specialists__selector__location-input']"
+)
+location.send_keys("Amsterdam")
+
+location.send_keys(Keys.ARROW_DOWN)
+location.send_keys(Keys.ENTER)
+
+"""Set specialisation to something"""
+# Select the correct specialisation
+specialisation_button = d.find_element(
+    By.XPATH, "//button[@id='specialists__selector__specialism-button']"
+)
+specialisation_button.click()
+
+specialisations = d.find_elements(By.CLASS_NAME, "specialism__option")
+specialisations[3].click()
+
+
+"""Change distance to Heel Nederland"""
+afstand_button = d.find_element(
+    By.XPATH, "//button[@id='specialists__selector__distance-button']"
+)
+afstand_button.click()
+
+# Select Heel Nederland
+afstand = d.find_elements(By.CLASS_NAME, "distance")
+
+afstanden = d.find_elements(By.CLASS_NAME, "distance__option")
+afstanden[3].click()
+# afstand[0].click()
+
+delay = 3  # seconds
+try:
+    myElem = WebDriverWait(d, delay).until(
+        EC.presence_of_element_located((By.XPATH, "//button[@class='cell cell--name']"))
+    )
+    data_entries = d.find_elements(By.XPATH, "//button[@class='cell cell--name']")
+    print(len(data_entries))
+    print("Page is ready!")
+except TimeoutException:
+    print("Loading took too much time!")
+
+
+# print(d[0])
+while True:
+    pass
